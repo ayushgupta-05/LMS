9745d14544a9c320b02bf5734077644d9dbbf9a3
Ayush Gupta <login2ayushgupta@gmail.com>
2025-09-16 12:14:21 +0530

    fixing errors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,32 @@
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { AppContextProvider } from './context/AppContext'
+// import { BrowserRouter } from 'react-router-dom' 
+// import {ClerkProvider} from '@clerk/clerk-react'
+
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+// if (!PUBLISHABLE_KEY) {
+//   throw new Error('Missing Publishable Key')
+// }
+
+// createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//   <ClerkProvider publishableKey={PUBLISHABLE_KEY} 
+//     afterSignOutUrl='/'  >
+//       <AppContextProvider>
+//       <App />
+//       </AppContextProvider>
+//   </ClerkProvider>
+//   </BrowserRouter>
+// )
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext'
 import { BrowserRouter } from 'react-router-dom' 
-import {ClerkProvider} from '@clerk/clerk-react'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -12,12 +35,11 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} 
-    afterSignOutUrl='/'  >
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <BrowserRouter>
       <AppContextProvider>
-      <App />
+        <App />
       </AppContextProvider>
+    </BrowserRouter>
   </ClerkProvider>
-  </BrowserRouter>
 )
